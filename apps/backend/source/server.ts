ecdcb8a78d6b40f0c3e8c7fb9b9ef007859eb260
Shahana Farooqui <shahana.farooqui@gmail.com>
2023-03-28 20:50:00 -0700

    Manifest CSP error fix

--- apps/backend/source/server.ts
+++ apps/backend/source/server.ts
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
   res.setHeader('Cache-Control', 'no-cache');
   res.setHeader(
     'Content-Security-Policy',
-    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; frame-src 'self'; style-src 'self'; manifest-src 'self';",
+    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; frame-src 'self'; style-src 'self'; manifest-src 'self' http://umbrel.local;",
   );
   next();
 });
