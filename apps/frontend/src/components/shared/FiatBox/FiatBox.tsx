d1efc3eda2617971bf3ea2c35d4eab0860f8eae4
Shahana Farooqui <shahana.farooqui@gmail.com>
2023-05-22 12:44:56 -0700

    Version updated to 0.0.4 and tiny CSS bug fixes
    Version updated to 0.0.4 and tiny CSS bug fixes

--- apps/frontend/src/components/shared/FiatBox/FiatBox.tsx
+++ apps/frontend/src/components/shared/FiatBox/FiatBox.tsx
@@ -7,7 +7,7 @@ import { Units } from '../../../utilities/constants';
 const FiatBox = props => {
   return (
     <span className={'d-flex align-items-center justify-content-start fiat-box-span ' + props.className}>
-      { props.symbol ? <FontAwesomeIcon icon={props.symbol} className={'svg-white fa-' + (props.iconSize || 'sm')} /> : <></> }
+      { props.symbol ? <FontAwesomeIcon icon={props.symbol} className={'fa-' + (props.iconSize || 'sm')} /> : <></> }
       <span className='ms-2px pt-2px'>{formatFiatValue((+props.value || 0), +props.rate, (props.fromUnit || Units.SATS))}</span>
     </span>
   );
