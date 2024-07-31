4e7b7c0b671d2e5b1549d6b78c1036728c49e358
afxsoftware <33781025+afxsoftware@users.noreply.github.com>
2024-07-31 17:57:39 -0400

    Change invoice and offer to lowercase
    Cannot send payment to an all uppercase lightning address like those created by the Phoenix wallet on iOS. Validation fails.
    
    Fixes issue #66.

--- apps/frontend/src/hooks/use-input.ts
+++ apps/frontend/src/hooks/use-input.ts
@@ -1,13 +1,26 @@
 import { useState } from 'react';
+import { InputType } from '../utilities/constants';
 
-const useInput = (validateValue) => {
+const useInput = (validateValue, inputType: InputType = InputType.ORIGINAL) => {
   const [enteredValue, setEnteredValue] = useState('');
   const [isTouched, setIsTouched] = useState(false);
+
+  const normalizeValue = (value: string) => {
+    switch (inputType) {
+      case 'lowercase':
+        return value.toLowerCase();
+      case 'uppercase':
+        return value.toUpperCase();
+      default:
+        return value;
+    }
+  };
 
   const valueIsValid = validateValue(enteredValue);
   const hasError = !valueIsValid && isTouched;
 
   const valueChangeHandler = (event) => {
+    event.target.value = normalizeValue(event.target.value);
     setEnteredValue(event.target.value);
   };
 
