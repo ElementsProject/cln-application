c8fafe0717a0412211a4c59337b79a932fbd75cc
ShahanaFarooqui <shahana.farooqui@gmail.com>
2024-09-06 19:13:14 -0700

    Adding mock use-http hook

--- apps/frontend/src/setupTests.ts
+++ apps/frontend/src/setupTests.ts
@@ -1,1 +1,8 @@
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom';
+jest.mock('./hooks/use-http.ts', () => require('./utilities/test-use-http.tsx'));
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(),
+}));
