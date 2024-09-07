3a47ff5708bc4d7c69359e92f363121e26d7a10f
ShahanaFarooqui <shahana.farooqui@gmail.com>
2024-09-06 19:11:19 -0700

    Update jsx camelCase property name

--- apps/frontend/src/svgs/Add.tsx
+++ apps/frontend/src/svgs/Add.tsx
@@ -11,8 +11,8 @@ export const AddSVG = props => {
       overlay={(props.showTooltip ? <Tooltip>{props.tooltipText || ''}</Tooltip> : <></>)}
     >
       <svg width='17' height='17' viewBox='0 0 17 17' fill='none' xmlns='http://www.w3.org/2000/svg'>
-        <path d='M1 8.5H16' stroke='#9F9F9F' stroke-width='1.3' stroke-linecap='round' stroke-linejoin='round' />
-        <path d='M8.5 1V16' stroke='#9F9F9F' stroke-width='1.3' stroke-linecap='round' stroke-linejoin='round' />
+        <path d='M1 8.5H16' stroke='#9F9F9F' strokeWidth='1.3' strokeLinecap='round' strokeLinejoin='round' />
+        <path d='M8.5 1V16' stroke='#9F9F9F' strokeWidth='1.3' strokeLinecap='round' strokeLinejoin='round' />
       </svg>
     </OverlayTrigger>
   );
