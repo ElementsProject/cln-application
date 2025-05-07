8afbc873b99928fe79594273990686a34a4d78cb
evansmj <evansmj500@gmail.com>
2025-05-06 20:05:49 -0700

    BKPR SVGs

---
+++ apps/frontend/src/svgs/ChevronDown.tsx
@@ -0,0 +1,19 @@
+export const ChevronDown = (props) => {
+  return (
+    <svg
+      className={props.className}
+      width={props.width}
+      height={props.height}
+      viewBox="0 0 12 7"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M0.816895 0.640839L5.85211 5.94219L10.8873 0.640839"
+        className="stroke-light"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
