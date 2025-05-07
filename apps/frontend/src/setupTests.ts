594de4b93d570717403df7dd1c9240f974e1fb82
evansmj <evansmj500@gmail.com>
2025-05-06 20:17:25 -0700

    Unit tests setup

--- apps/frontend/src/setupTests.ts
+++ apps/frontend/src/setupTests.ts
@@ -1,8 +1,58 @@
 import '@testing-library/jest-dom';
-import '@testing-library/jest-dom';
-jest.mock('./hooks/use-http.ts', () => require('./utilities/test-use-http.tsx'));
+import { spyOnBKPRGetAccountEvents, spyOnBKPRGetSatsFlow, spyOnBKPRGetVolume, spyOnGetInfo, spyOnListChannels, spyOnListFunds, spyOnListNodes, spyOnListPeers } from './utilities/test-utilities/mockService';
 
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useNavigate: jest.fn(),
-}));
+let mockedLocation = {
+  pathname: '/',
+  search: '',
+  hash: '',
+  state: null,
+  key: 'default',
+};
+
+jest.mock('react-router-dom', () => {
+  const actual = jest.requireActual('react-router-dom');
+  return {
+    ...actual,
+    useNavigate: () => jest.fn(),
+    useLocation: () => mockedLocation,
+  };
+});
+
+export const setMockedLocation = (location: Partial<Location>) => {
+  mockedLocation = { ...mockedLocation, ...location };
+};
+
+beforeEach(() => {
+  jest.useFakeTimers();
+  spyOnGetInfo();
+  spyOnListNodes();
+  spyOnListChannels();
+  spyOnListPeers();
+  spyOnListFunds();
+  spyOnBKPRGetAccountEvents();
+  spyOnBKPRGetSatsFlow();
+  spyOnBKPRGetVolume();
+  const originalConsoleWarning = console.warn;
+  jest.spyOn(console, 'warn').mockImplementation((msg, ...args) => {
+    if (
+      typeof msg === 'string' &&
+      msg.includes('React Router Future Flag Warning: React Router will begin wrapping state updates in `React.startTransition` in v7')
+    ) {
+      return;
+    }
+    originalConsoleWarning(msg, ...args);
+  });
+});
+
+afterEach(() => {
+  jest.useRealTimers();
+  jest.restoreAllMocks();
+});
+
+beforeAll(() => {
+  window.scrollTo = jest.fn();
+});
+
+afterAll(() => {
+  jest.restoreAllMocks();
+});
