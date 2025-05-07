b694b9cbbd295343e497151379d5059bc7897a31
evansmj <evansmj500@gmail.com>
2025-05-06 20:05:49 -0700

    BKPR SVGs

---
+++ apps/frontend/src/svgs/AccountEvents.tsx
@@ -0,0 +1,43 @@
+export const AccountEventsSVG = props => {
+  return (
+    <svg
+      className={props.className}
+      width="52"
+      height="52"
+      style={{ minWidth: "52px" }}
+      viewBox="0 0 62 62"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="30" cy="30" r="15" className="stroke-primary-bg stroke-width-28" />
+      <path
+        d="M19.3594 43.0625V32.6562H27.4531"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="stroke-white-primary"
+      />
+      <path
+        d="M45.9531 43.0625H17.0469"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="stroke-white-primary"
+      />
+      <path
+        d="M27.4531 43.0625V25.7188H35.5469"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="stroke-white-primary"
+      />
+      <path
+        d="M43.6406 18.7812H35.5469V43.0625H43.6406V18.7812Z"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="stroke-white-primary"
+      />
+    </svg>
+  );
+};
