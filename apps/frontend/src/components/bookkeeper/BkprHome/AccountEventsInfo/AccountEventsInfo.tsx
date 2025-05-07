85f47d5a595e446a435a1a2a316adca4c533d8e1
evansmj <evansmj500@gmail.com>
2025-05-06 20:32:58 -0700

    BKPR Dashboard

---
+++ apps/frontend/src/components/bookkeeper/BkprHome/AccountEventsInfo/AccountEventsInfo.tsx
@@ -0,0 +1,76 @@
+import './AccountEventsInfo.scss';
+import { useNavigate } from 'react-router-dom';
+import { Card, Row } from 'react-bootstrap';
+import { ActionSVG } from '../../../../svgs/Action';
+import { AccountEventsSVG } from '../../../../svgs/AccountEvents';
+import { CallStatus, Units } from '../../../../utilities/constants';
+import CurrencyBox from '../../../shared/CurrencyBox/CurrencyBox';
+import StatusAlert from '../../../shared/StatusAlert/StatusAlert';
+import { useSelector } from 'react-redux';
+import { selectIsSatsFlowLoading, selectSatsFlowError, selectSummary } from '../../../../store/bkprSelectors';
+
+const AccountEventsInfo = () => {
+  const navigate = useNavigate();
+  const bkprSummary = useSelector(selectSummary);
+  const satsFlowLoading = useSelector(selectIsSatsFlowLoading);
+  const satsFlowError = useSelector(selectSatsFlowError);
+
+  return (
+    <Card className='h-100 inner-box-shadow px-4 pt-4 pb-3'>
+      <Card.Header className='d-flex px-1 fs-18px p-0 fw-bold text-dark'>
+        <AccountEventsSVG className='me-3' />
+        <Row>
+          <span className='fw-bold'>Account Events</span>
+          <span className='text-muted fs-7'>Track account activity at any given time.</span>
+        </Row>
+      </Card.Header>
+      <Card.Body className='mt-2 p-0'>
+        {satsFlowLoading ?
+          <span className='h-100 d-flex justify-content-center align-items-center text-light fs-6 delayed-load'>
+            No account activity found. Send/Receive payments to see details!
+          </span>
+          :
+          satsFlowError ?
+            <StatusAlert responseStatus={CallStatus.ERROR} responseMessage={satsFlowError.replace('Satsflow: ', '')} />
+            :
+            <>
+              <div className='d-flex flex-column align-items-stretch justify-content-between'>
+                <div className='d-flex flex-column align-items-start p-2'>
+                  <span className='fs-6 text-dark'>Total Invoice Received</span>
+                  <span className='fs-2 fw-bold primary'>+<CurrencyBox value={bkprSummary.total_invoice_received_msat} shorten={false} hideUnit={true} fromUnit={Units.MSATS} rootClasses='d-inline-flex flex-column' currencyClasses='fs-2 fw-bold primary'></CurrencyBox></span>
+                </div>
+                <div className='d-flex flex-column align-items-start p-2'>
+                  <span className='fs-6 text-dark'>Total Payments Sent</span>
+                  <span className='fs-2 fw-bold primary'>-<CurrencyBox value={bkprSummary.total_payments_sent_msat} shorten={false} hideUnit={true} fromUnit={Units.MSATS} rootClasses='d-inline-flex flex-column' currencyClasses='fs-2 fw-bold primary'></CurrencyBox></span>
+                </div>
+              </div>
+              <div className='d-flex flex-column align-items-stretch justify-content-between'>
+                <div className='d-flex flex-column align-items-start p-2'>
+                  <span className='fs-6 text-dark'>Routing Revenue</span>
+                  <span className='fs-2 fw-bold primary'>+<CurrencyBox value={bkprSummary.routing_revenue_msat} shorten={false} hideUnit={true} fromUnit={Units.MSATS} rootClasses='d-inline-flex flex-column' currencyClasses='fs-2 fw-bold primary'></CurrencyBox></span>
+                </div>
+                <div className='d-flex flex-column align-items-start p-2'>
+                  <span className='fs-6 text-dark'>On-Chain Fee</span>
+                  <span className='fs-2 fw-bold primary'>-<CurrencyBox value={bkprSummary.onchain_fee_msat} shorten={false} hideUnit={true} fromUnit={Units.MSATS} rootClasses='d-inline-flex flex-column' currencyClasses='fs-2 fw-bold primary'></CurrencyBox></span>
+                </div>
+              </div>
+            </>
+        }
+      </Card.Body>
+      <Card.Footer className='d-flex justify-content-end'>
+        <button
+          tabIndex={1}
+          type='button'
+          disabled
+          className='btn-rounded bg-primary fs-6'
+          onClick={() => navigate('./accountevents')}
+        >
+          View More
+          <ActionSVG className='ms-3' />
+        </button>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+export default AccountEventsInfo;
---
+++ apps/frontend/src/components/bookkeeper/BkprHome/VolumeInfo/VolumeInfo.tsx
@@ -0,0 +1,90 @@
+import './VolumeInfo.scss';
+import { useNavigate } from 'react-router-dom';
+import { Card, Col, OverlayTrigger, Row, Tooltip } from 'react-bootstrap';
+import { ActionSVG } from '../../../../svgs/Action';
+import { VolumeChartSVG } from '../../../../svgs/VolumeChart';
+import { formatCurrency } from '../../../../utilities/data-formatters';
+import { CallStatus, Units } from '../../../../utilities/constants';
+
+import StatusAlert from '../../../shared/StatusAlert/StatusAlert';
+import { useSelector } from 'react-redux';
+import { selectIsVolumeLoading, selectSummary, selectVolumeError } from '../../../../store/bkprSelectors';
+import { selectUIConfigUnit } from '../../../../store/rootSelectors';
+
+const SummaryRouteTooltip = ({ route, unit }: any) => {
+  return (
+    <Tooltip>
+      <Row className='p-1'>
+        <Col xs={12}>Aliases: {route?.channel_aliases}</Col>
+        <Col xs={12}>Fee Collected: {formatCurrency(route?.fee_msat, Units.MSATS, unit, false, 5, 'string')}</Col>
+      </Row>
+    </Tooltip>
+  );
+};
+
+const VolumeInfo = () => {
+  const navigate = useNavigate();
+  const uiConfigUnit = useSelector(selectUIConfigUnit);
+  const bkprSummary = useSelector(selectSummary);
+  const isVolumeLoading = useSelector(selectIsVolumeLoading);
+  const volumeError = useSelector(selectVolumeError);
+
+  return (
+    <Card className='volume-info inner-box-shadow p-4 pb-3'>
+      <Card.Header className='d-flex px-1 fs-18px p-0 fw-bold text-dark'>
+        <VolumeChartSVG className='me-3' />
+        <Row>
+          <span className='fw-bold'>Volume Chart</span>
+          <span className='text-muted fs-7'>Track route performance.</span>
+        </Row>
+      </Card.Header>
+      <Card.Body className='mt-2 p-0'>
+        {isVolumeLoading ?
+          <span className='h-100 d-flex justify-content-center align-items-center text-light fs-6 delayed-load'>
+            No forwarding activity found. Forward transactions to see details!
+          </span>
+          :
+          volumeError ?
+            <StatusAlert responseStatus={CallStatus.ERROR} responseMessage={volumeError.replace('Volume: ', '')} />
+            :
+            <>
+              <div className='d-flex flex-column align-items-start p-2 pb-0 h-50'>
+                <span className='fs-6 text-dark'>Route with Most Traffic</span>
+                <OverlayTrigger
+                  placement='auto'
+                  delay={{ show: 250, hide: 250 }}
+                  overlay={SummaryRouteTooltip({ route: bkprSummary.most_traffic_route, unit: uiConfigUnit })}
+                >
+                  <span className='fs-4 fw-bold positive'>{bkprSummary.most_traffic_route?.channel_scids}</span>
+                </OverlayTrigger>
+              </div>
+              <div className='d-flex flex-column align-items-start p-2 pt-0 h-50'>
+                <span className='fs-6 text-dark'>Route with Least Traffic</span>
+                <OverlayTrigger
+                  placement='auto'
+                  delay={{ show: 250, hide: 250 }}
+                  overlay={SummaryRouteTooltip({ route: bkprSummary.least_traffic_route, unit: uiConfigUnit })}
+                >
+                  <span className='fs-4 fw-bold negative'>{bkprSummary.least_traffic_route?.channel_scids}</span>
+                </OverlayTrigger>
+              </div>
+            </>
+        }
+      </Card.Body>
+      <Card.Footer className='d-flex justify-content-end'>
+        <button
+          tabIndex={1}
+          type='button'
+          disabled
+          className='btn-rounded bg-primary fs-6 me-3'
+          onClick={() => navigate('./volume')}
+        >
+          View More
+          <ActionSVG className='ms-3' />
+        </button>
+      </Card.Footer>
+    </Card>
+  );
+};
+
+export default VolumeInfo;
