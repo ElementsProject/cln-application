69ec6b0e552592e665099b0cde67b052f4ce790c
ShahanaFarooqui <shahana.farooqui@gmail.com>
2026-03-05 14:20:25 -0800

    Add Custom Fee Rate for Channel Opening

--- apps/frontend/src/components/cln/ChannelOpen/ChannelOpen.tsx
+++ apps/frontend/src/components/cln/ChannelOpen/ChannelOpen.tsx
@@ -1,6 +1,6 @@
 import './ChannelOpen.scss';
-import { useState } from 'react';
-import { motion } from 'framer-motion';
+import { useState, ChangeEvent } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import { Spinner, Card, Row, Col, Form, InputGroup } from 'react-bootstrap';
 
 import logger from '../../../services/logger.service';
@@ -9,6 +9,7 @@ import { CallStatus, FeeRate, BOUNCY_SPRING_VARIANTS_1, CLEAR_STATUS_ALERT_DELAY
 import { ActionSVG } from '../../../svgs/Action';
 import { AmountSVG } from '../../../svgs/Amount';
 import { AddressSVG } from '../../../svgs/Address';
+import { FeeRateSVG } from '../../../svgs/FeeRate';
 import FiatBox from '../../shared/FiatBox/FiatBox';
 import InvalidInputMessage from '../../shared/InvalidInputMessage/InvalidInputMessage';
 import { CloseSVG } from '../../../svgs/Close';
@@ -24,10 +25,12 @@ const ChannelOpen = (props) => {
   const walletBalances = useSelector(selectWalletBalances);
   const [selFeeRate, setSelFeeRate] = useState(FeeRate.NORMAL);
   const [announce, setAnnounce] = useState(true);
+  const [showCustomFeeRate, setShowCustomFeeRate] = useState(false);
   const [responseStatus, setResponseStatus] = useState(CallStatus.NONE);
   const [responseMessage, setResponseMessage] = useState('');
   const isValidAmount = (value) => value.trim() !== '' && value > 0 && value <= (walletBalances.btcSpendableBalance || 0);
   const isValidPubkey = (value) => value.includes('@') && value.includes(':');
+  const isValidFeeRate = (value) => !showCustomFeeRate || (value.trim() !== '' && value > 0);
 
   const {
     value: pubkeyValue,
@@ -45,10 +48,18 @@ const ChannelOpen = (props) => {
     inputBlurHandler: amountBlurHandler,
     reset: resetAmount,
   } = useInput(isValidAmount);
+  const {
+    value: feeRateValue,
+    isValid: feeRateIsValid,
+    hasError: feeRateHasError,
+    valueChangeHandler: feeRateChangeHandler,
+    inputBlurHandler: feeRateBlurHandler,
+    reset: resetFeeRate,
+  } = useInput(isValidFeeRate);
 
   let formIsValid = false;
 
-  if (pubkeyIsValid && amountIsValid) {
+  if (pubkeyIsValid && amountIsValid && feeRateIsValid) {
     formIsValid = true;
   }
 
@@ -69,11 +80,13 @@ const ChannelOpen = (props) => {
   const touchFormControls = () => {
     pubkeyBlurHandler();
     amountBlurHandler();
+    feeRateBlurHandler();
   };
 
   const resetFormValues = () => {
     resetPubkey();
     resetAmount();
+    resetFeeRate();
     setAnnounce(true);
     setSelFeeRate(FeeRate.NORMAL);
   };
@@ -89,6 +102,10 @@ const ChannelOpen = (props) => {
     }, CLEAR_STATUS_ALERT_DELAY);
   };
 
+  const showCustomFeeRateChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    setShowCustomFeeRate(event.target.checked);
+  };
+
   const ChannelOpenHandler = event => {
     event.preventDefault();
     touchFormControls();
@@ -97,16 +114,16 @@ const ChannelOpen = (props) => {
     }
     setResponseStatus(CallStatus.PENDING);
     setResponseMessage('Opening Channel...');
-    CLNService.openChannel(pubkeyValue, +amountValue, selFeeRate.toLowerCase(), announce)
+    CLNService.openChannel(pubkeyValue, +amountValue, (!!showCustomFeeRate ? feeRateValue + 'perkw' : selFeeRate.toLowerCase()), announce)
       .then((response: any) => {
         logger.info(response);
         if (response.channel_id || response.txid) {
           setResponseStatus(CallStatus.SUCCESS);
           setResponseMessage(
             'Channel opened with ' +
-              (response.channel_id
-                ? 'channel id ' + response.channel_id
-                : 'transaction id ' + response.txid),
+            (response.channel_id
+              ? 'channel id ' + response.channel_id
+              : 'transaction id ' + response.txid),
           );
           resetFormValues();
           delayedClearStatusAlert(true, response.channel_id);
@@ -128,93 +145,160 @@ const ChannelOpen = (props) => {
     <form onSubmit={ChannelOpenHandler} className='h-100' data-testid='channel-open'>
       <Card className='h-100 d-flex align-items-stretch' data-testid='channel-open-card'>
         <Card.Body className='text-dark d-flex align-items-stretch flex-column pt-4'>
-            <Card.Header className='p-0 d-flex align-items-start justify-content-between'>
-              <div className='fs-4 p-0 fw-bold text-dark'>
-                Open Channel
-              </div>
-              <span className='span-close-svg' onClick={props.onClose}><CloseSVG /></span>
-            </Card.Header>
-            <Card.Body className='pb-0 px-1 d-flex flex-column align-items-start justify-content-between'>
-              <Row className='d-flex align-items-start justify-content-center'>
-                <Col xs={12}>
-                  <Form.Label className='text-dark'>Node ID*</Form.Label>
-                  <InputGroup className={(pubkeyHasError ? 'invalid' : '')}>
-                    <InputGroup.Text className='form-control-addon form-control-addon-left'>
-                      <AddressSVG />
-                    </InputGroup.Text>
-                    <Form.Control
-                      tabIndex={1}
-                      id='pubkey'
-                      type='text'
-                      placeholder='Pubkey@Ip:Port'
-                      aria-label='pubkey'
-                      aria-describedby='addon-pubkey'
-                      className='form-control-right'
-                      value={pubkeyValue}
-                      onChange={pubkeyChangeHandler}
-                      onBlur={pubkeyBlurHandler}
-                    />
-                  </InputGroup>
-                  {(pubkeyHasError) ?
-                    <InvalidInputMessage message={'Invalid Node ID'} /> : <div className='message'></div>
-                  }
-                </Col>
-                <Col xs={12}>
-                  <Form.Label className='text-dark'>Amount*</Form.Label>
-                  <InputGroup className={(amountHasError ? 'invalid ' : '')}>
-                    <InputGroup.Text className='form-control-addon form-control-addon-left'>
-                      <AmountSVG />
-                    </InputGroup.Text>
-                    <Form.Control
-                      tabIndex={2}
-                      id='amount'
-                      type='number'
-                      placeholder={'Amount (Between 1 - ' + parseFloat((walletBalances.btcSpendableBalance || 0).toString()).toLocaleString('en-us')  + ' Sats)'}
-                      aria-label='amount'
-                      aria-describedby='addon-amount'
-                      className='form-control-right'
-                      value={amountValue}
-                      onChange={amountChangeHandler}
-                      onBlur={amountBlurHandler}
-                    />
-                  </InputGroup>
-                  {
-                    !amountHasError ?
-                      amountValue ?
-                        <p className='fs-7 text-light d-flex align-items-center justify-content-end'>
-                          ~ <FiatBox value={(+amountValue || 0)} fiatUnit={fiatUnit} symbol={fiatConfig.symbol} rate={fiatConfig.rate} />
-                        </p>
+          <Card.Header className='p-0 d-flex align-items-start justify-content-between'>
+            <div className='fs-4 p-0 fw-bold text-dark'>
+              Open Channel
+            </div>
+            <span className='span-close-svg' onClick={props.onClose}><CloseSVG /></span>
+          </Card.Header>
+          <Card.Body className='pb-0 px-1 d-flex flex-column align-items-start justify-content-between'>
+            <Row className='d-flex align-items-start justify-content-center'>
+              <Col xs={12}>
+                <Form.Label className='text-dark'>Node ID*</Form.Label>
+                <InputGroup className={(pubkeyHasError ? 'invalid' : '')}>
+                  <InputGroup.Text className='form-control-addon form-control-addon-left'>
+                    <AddressSVG />
+                  </InputGroup.Text>
+                  <Form.Control
+                    tabIndex={1}
+                    id='pubkey'
+                    type='text'
+                    placeholder='Pubkey@Ip:Port'
+                    aria-label='pubkey'
+                    aria-describedby='addon-pubkey'
+                    data-testid='pubkey'
+                    className='form-control-right'
+                    value={pubkeyValue}
+                    onChange={pubkeyChangeHandler}
+                    onBlur={pubkeyBlurHandler}
+                  />
+                </InputGroup>
+                {(pubkeyHasError) ?
+                  <InvalidInputMessage message={'Invalid Node ID'} /> : <div className='message'></div>
+                }
+              </Col>
+              <Col xs={12}>
+                <Form.Label className='text-dark'>Amount*</Form.Label>
+                <InputGroup className={(amountHasError ? 'invalid ' : '')}>
+                  <InputGroup.Text className='form-control-addon form-control-addon-left'>
+                    <AmountSVG />
+                  </InputGroup.Text>
+                  <Form.Control
+                    tabIndex={2}
+                    id='amount'
+                    type='number'
+                    placeholder={'Amount (Between 1 - ' + parseFloat((walletBalances.btcSpendableBalance || 0).toString()).toLocaleString('en-us') + ' Sats)'}
+                    aria-label='amount'
+                    aria-describedby='addon-amount'
+                    className='form-control-right'
+                    value={amountValue}
+                    onChange={amountChangeHandler}
+                    onBlur={amountBlurHandler}
+                    data-testid='amount'
+                  />
+                </InputGroup>
+                {
+                  !amountHasError ?
+                    amountValue ?
+                      <p className='fs-7 text-light d-flex align-items-center justify-content-end'>
+                        ~ <FiatBox value={(+amountValue || 0)} fiatUnit={fiatUnit} symbol={fiatConfig.symbol} rate={fiatConfig.rate} />
+                      </p>
                       :
-                        <p className='message'></p>
+                      <p className='message'></p>
                     :
-                      <InvalidInputMessage message={
-                        (+amountValue <= 0) ? 
-                          'Amount should be greater than 0'
-                        : (+amountValue > (walletBalances.btcSpendableBalance || 0)) ? 
+                    <InvalidInputMessage message={
+                      (+amountValue <= 0) ?
+                        'Amount should be greater than 0'
+                        : (+amountValue > (walletBalances.btcSpendableBalance || 0)) ?
                           'Amount should be lesser then ' + (walletBalances.btcSpendableBalance || 0)
-                        :
+                          :
                           'Invalid Amount'
-                      } />
-                  }
-                </Col>
-                <Col xs={12} className='d-flex align-items-center mb-3'>
-                  <Form.Label className='text-dark me-4'>Announce</Form.Label>
-                  <div tabIndex={3} className='switch' data-isswitchon={announce} onClick={() => setAnnounce(!announce)}>
-                    <motion.div className='handle' layout transition={BOUNCY_SPRING_VARIANTS_1} />
-                  </div>
-                </Col>
-                <Col xs={12}>
-                  <FeerateRange tabIndex={4} selFeeRate={selFeeRate} selFeeRateChangeHandler={selFeeRateChangeHandler} />
-                </Col>
-              </Row>
-              <StatusAlert responseStatus={responseStatus} responseMessage={responseMessage} />
-            </Card.Body>
-            <Card.Footer className='d-flex justify-content-center'>
-              <button tabIndex={5} type='submit' className='btn-rounded bg-primary' disabled={responseStatus === CallStatus.PENDING}>
-                Open Channel
-                {responseStatus === CallStatus.PENDING ? <Spinner className='mt-1 ms-2 text-white-dark' size='sm' /> : <ActionSVG className='ms-3' />}
-              </button>
-            </Card.Footer>
+                    } />
+                }
+              </Col>
+              <Col xs={12} className='d-flex align-items-center mb-3'>
+                <Form.Label className='text-dark me-4'>Announce</Form.Label>
+                <div tabIndex={3} className='switch' data-testid='announce-switch' data-isswitchon={announce} onClick={() => setAnnounce(!announce)}>
+                  <motion.div className='handle' layout transition={BOUNCY_SPRING_VARIANTS_1} />
+                </div>
+              </Col>
+              <Col xs={12} className='d-flex align-items-center mb-3'>
+                <Form.Check
+                  tabIndex={4}
+                  onChange={showCustomFeeRateChangeHandler}
+                  checked={showCustomFeeRate}
+                  inline
+                  className='text-dark'
+                  label='Custom Fee Rate'
+                  name='showCustomFeeRate'
+                  type='checkbox'
+                  id='showCustomFeeRateCheckbox'
+                  data-testid='show-custom-fee-rate'
+                />
+              </Col>
+
+              <AnimatePresence mode="wait">
+                {showCustomFeeRate ? (
+                  <motion.div
+                    key="custom-fee-rate"
+                    initial={{ opacity: 0, y: -10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 10 }}
+                    transition={{ duration: 0.2 }}
+                    style={{ width: '100%' }}
+                  >
+                    <Col xs={12}>
+                      <Form.Label className='text-dark'>Fee Rate*</Form.Label>
+                      <InputGroup className={(feeRateHasError ? 'invalid ' : '')}>
+                        <InputGroup.Text className='form-control-addon form-control-addon-left'>
+                          <FeeRateSVG />
+                        </InputGroup.Text>
+                        <Form.Control
+                          tabIndex={5}
+                          id='feeRate'
+                          type='number'
+                          placeholder={'Fee Rate'}
+                          aria-label='feeRate'
+                          aria-describedby='addon-feeRate'
+                          className='form-control-middle'
+                          value={feeRateValue}
+                          onChange={feeRateChangeHandler}
+                          onBlur={feeRateBlurHandler}
+                          data-testid='fee-rate-input'
+                        />
+                        <InputGroup.Text data-testid='fee-rate-unit' id='Fee Rate Unit' className='form-control-addon form-control-addon-right text-light'>
+                          perkw
+                        </InputGroup.Text>
+                      </InputGroup>
+                      {(feeRateHasError) ?
+                        <InvalidInputMessage message={(+feeRateValue <= 0) ? 'Fee Rate should be greater than 0' : 'Invalid Fee Rate'} /> : <div className='message'></div>
+                      }
+                    </Col>
+                  </motion.div>
+                ) : (
+                  <motion.div
+                    key="fee-rate-range"
+                    initial={{ opacity: 0, y: -10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    exit={{ opacity: 0, y: 10 }}
+                    transition={{ duration: 0.2 }}
+                    style={{ width: '100%' }}
+                  >
+                    <Col xs={12}>
+                      <FeerateRange tabIndex={6} selFeeRate={selFeeRate} selFeeRateChangeHandler={selFeeRateChangeHandler} />
+                    </Col>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </Row>
+            <StatusAlert responseStatus={responseStatus} responseMessage={responseMessage} />
+          </Card.Body>
+          <Card.Footer className='d-flex justify-content-center'>
+            <button tabIndex={5} data-testid='button-open-channel-submit' type='submit' className='btn-rounded bg-primary' disabled={responseStatus === CallStatus.PENDING}>
+              Open Channel
+              {responseStatus === CallStatus.PENDING ? <Spinner className='mt-1 ms-2 text-white-dark' size='sm' /> : <ActionSVG className='ms-3' />}
+            </button>
+          </Card.Footer>
         </Card.Body>
       </Card>
     </form>
